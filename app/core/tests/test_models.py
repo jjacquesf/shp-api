e62b457b324ce81e7bbc8da129dc3c24e19b03a6
Jorge Jacques <jjacques@gmail.com>
2023-10-28 06:34:19 -0600

    Validate no empty email for new users

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -6,9 +6,9 @@ from django.contrib.auth import get_user_model
 
 
 class ModelTests(TestCase):
-        """Test models"""
+      """Test models"""
 
-        def test_create_user_with_email_successful(self):
+      def test_create_user_with_email_successful(self):
             """Test creating a user with an email is successful"""
             email = 'test@example.com'
             password = 'testpass123'
@@ -20,16 +20,21 @@ class ModelTests(TestCase):
             self.assertEqual(user.email, email)
             self.assertTrue(user.check_password(password))
 
-        def test_new_user_email_normalized(self):
+      def test_new_user_email_normalized(self):
             """Test email is normalized for new users."""
             sample_emails = [
-                 ['test1@EXAMPLE.com', 'test1@example.com'],
-                 ['Test2@Example.com', 'Test2@example.com'],
-                 ['TEST3@EXAMPLE.com', 'TEST3@example.com'],
-                 ['test4@example.COM', 'test4@example.com'],
+                  ['test1@EXAMPLE.com', 'test1@example.com'],
+                  ['Test2@Example.com', 'Test2@example.com'],
+                  ['TEST3@EXAMPLE.com', 'TEST3@example.com'],
+                  ['test4@example.COM', 'test4@example.com'],
             ]
 
             for email, expected in sample_emails:
-                 user = get_user_model().objects.create_user(email, 'sample123')
-                 self.assertEqual(user.email, expected)
+                  user = get_user_model().objects.create_user(email, 'sample123')
+                  self.assertEqual(user.email, expected)
+
+      def test_new_user_without_email_error(self):
+            """Test taht creating an user without an email raises a ValueError"""
+            with self.assertRaises(ValueError):
+                  get_user_model().objects.create_user('', 'test123')
 
