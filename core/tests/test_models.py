17e31821ecf6b100208f1a8e9ad456be539d978f
Jorge Jacques <jjacquesf@gmail.com>
2024-01-07 18:32:30 -0600

    Add test fro all catalog models

--- core/tests/test_models.py
+++ core/tests/test_models.py
@@ -53,6 +53,59 @@ class ModelTests(TestCase):
             """Test creating a municipality"""
             name = "Zapopan"
             municipality = models.Municipality.objects.create(
-                  name="Zapopan"
+                  name=name
             )
             self.assertEqual(municipality.name, name)
+
+      def test_create_entity(self):
+            """Test creating a entity"""
+            name = "Entity name"
+            entity = models.Entity.objects.create(
+                  name=name
+            )
+            self.assertEqual(entity.name, name)
+
+
+      def test_create_institution(self):
+            name = "Institution name"
+            institution = models.Institution.objects.create(
+                  name=name
+            )
+            self.assertEqual(institution.name, name)
+
+      def test_create_stateorg(self):
+            """Test creating a stateorg"""
+            name = "Name"
+            stateorg = models.StateOrg.objects.create(
+                  name=name
+            )
+            self.assertEqual(stateorg.name, name)
+
+      def test_create_sifuser(self):
+            """Test creating a sifuser"""
+            stateorg = models.StateOrg.objects.create(name="state org")
+            name = "SIF user name"
+            sifuser = models.SifUser.objects.create(
+                  name=name,
+                  stateorg=stateorg
+            )
+            self.assertEqual(sifuser.name, name)
+
+      def test_create_sianuser(self):
+            """Test creating a sianuser"""
+            stateorg = models.StateOrg.objects.create(name="state org")
+            name = "SIAN user name"
+            sianuser = models.SianUser.objects.create(
+                  name=name,
+                  stateorg=stateorg
+            )
+            self.assertEqual(sianuser.name, name)
+
+
+      def test_create_supplier(self):
+            """Test creating a supplier"""
+            name = "Supplier name"
+            supplier = models.Supplier.objects.create(
+                  name=name
+            )
+            self.assertEqual(supplier.name, name)
