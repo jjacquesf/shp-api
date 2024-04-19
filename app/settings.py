f68529c6ecb8115f5b3fc53c2d7f3145746e8293
Jorge Jacques <jjacquesf@gmail.com>
2024-04-19 07:15:26 -0600

    Allow all origins

--- app/settings.py
+++ app/settings.py
@@ -30,10 +30,11 @@ ALLOWED_HOSTS = [
 ]
 
 
-# CORS_ORIGIN_ALLOW_ALL=True
-CORS_ORIGIN_WHITELIST = (
-    'http://localhost:5173',
-)
+CORS_ORIGIN_ALLOW_ALL=True
+# CORS_ORIGIN_WHITELIST = (
+#     'http://localhost:5173',
+#     'http://localhost:8080',
+# )
 
 # Application definition
 
