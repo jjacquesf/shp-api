e1957d2a607eb206e45ff0840d92a8a975b6c35a
Jorge Jacques <jjacquesf@gmail.com>
2024-04-23 09:57:53 -0600

    Update media root

--- app/settings.py
+++ app/settings.py
@@ -211,5 +211,5 @@ EAV2_PRIMARY_KEY_FIELD = "django.db.models.BigAutoField"
 #     }
 # }
 
-# MEDIA_ROOT = os.environ.get('MEDIA_ROOT')
-MEDIA_ROOT = "uploads/"
+MEDIA_ROOT = os.environ.get('MEDIA_ROOT')
+# MEDIA_ROOT = "uploads/"
