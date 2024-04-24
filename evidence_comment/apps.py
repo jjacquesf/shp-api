16b88763cb301e4a8e8ad1f1317c905fb1c852e8
Jorge Jacques <jjacquesf@gmail.com>
2024-04-24 16:18:16 -0600

    Add notifications for evidence findings and comments

--- evidence_comment/apps.py
+++ evidence_comment/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class EvidenceCommentsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'evidence_comment'
+
+
+    def ready(self):
+        import evidence_comment.signals
---
+++ evidence_comment/signals.py
@@ -0,0 +1,14 @@
+# code
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from core import models
+
+@receiver(post_save, sender=models.EvidenceComment) 
+def create(sender, instance, created, **kwargs):
+    if created:
+        models.Notification.objects.create(
+            evidence=instance.evidence,
+            user=instance.evidence.owner,
+            content=f'Nuevo comentario en {instance.evidence.type.name}'
+        )
